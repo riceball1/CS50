8b2acc609747ec23c021b62cde85a96d8abe284d
Dana Ng <ngdana1@gmail.com>
2017-05-03 16:02:29 -0700

    added additional pset - trying the more comfortable cases

---
+++ pset2/intials-comfortable.c
@@ -0,0 +1,22 @@
+/** 
+* Initals More Comfortable Details: 
+* http://docs.cs50.net/problems/initials/more/initials.html
+**/
+
+// declare libraries
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+
+// setup main
+int main(void) 
+{
+    // prompt user for their name
+    // use get_string
+    // user may have extra spaces between words in name
+
+    // output
+    // print initals with no spaces/periods and in Captialized letters 
+    // followed by \n
+}
